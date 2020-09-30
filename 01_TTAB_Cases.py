e57fc44645b6eceb22c1a7a444d8e0933ddfc963
Sean Nguyen <nguyens7@msu.edu>
2020-09-29 22:36:50 -0400

    XML EDA

--- 01_TTAB_Cases.py
+++ 01_TTAB_Cases.py
@@ -2,15 +2,31 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 
 
-root = ET.parse('Data/USPTO_TTAB_decisions.xml.xml').getroot()
+# tree = ET.parse('Documents/com~apple~CloudDocs/InfringeMark/Data.nosyc/USPTO_TTAB_decisions.xml')
 
-tags = {"tags":[]}
-for elem in root:
-    tag = {}
-    tag["Id"] = elem.attrib['Id']
-    tag["TagName"] = elem.attrib['TagName']
-    tag["Count"] = elem.attrib['Count']
-    tags["tags"]. append(tag)
+# root = tree.getroot()
 
-df_users = pd.DataFrame(tags["tags"])
-df_users.head()
+# print(root)
+
+# tags = {"tags":[]}
+# for elem in root:
+#     tag = {}
+#     tag["Id"] = elem.attrib['Id']
+#     tag["TagName"] = elem.attrib['TagName']
+#     tag["Count"] = elem.attrib['Count']
+#     tags["tags"]. append(tag)
+
+# df_users = pd.DataFrame(tags["tags"])
+# df_users.head()
+
+import traceback
+# ...
+try:
+    input_fname = "/Data.nosyc/USPTO_TTAB_decisions.xml"
+    tree.parse(input_fname)
+    # ...
+except IOError:
+    ex_info = traceback.format_exc()
+    print('ERROR!!! Cannot parse file: %s' % (input_fname))
+    print('ERROR!!! Check if this file exists and you have right to read it!')
+    print('ERROR!!! Exception info:\n%s' % (ex_info))
