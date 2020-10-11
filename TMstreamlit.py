2923e087c7e16df7b23799d827a324b0a9d62ed2
Sean Nguyen <nguyens7@msu.edu>
2020-10-10 21:29:36 -0400

    Fixed correlation colors

--- TMstreamlit.py
+++ TMstreamlit.py
@@ -32,6 +32,91 @@ import re
 from tqdm import tqdm
 from sklearn.preprocessing import MinMaxScaler
 
+def make_clickable(val):
+    	return '<a href="{}">{}</a>'.format(val,val)
+
+# load Model For Gender Prediction
+TM_GBC_model = open("Data.nosync/TM_Gradient_boost_base_model.pkl","rb")
+TM_clf = joblib.load(TM_GBC_model)
+
+	# Prediction
+def predict_TM_outcome(data):
+	result = TM_clf.predict(data)
+	data['GBC_predict'] = result
+	return data
+
+# Featurizer
+
+pshp_soundex_first = PSHPSoundexFirst()
+pe = Ainsworth()	
+iss = IterativeSubString()
+bisim = BISIM()
+dlev = DiscountedLevenshtein()
+prefix = Prefix()
+lcs = LCSstr()
+mlipns = MLIPNS()
+strcmp95 = Strcmp95()
+mra = MRA()
+editex = Editex()
+saps = SAPS()
+flexmetric = FlexMetric()
+jaro = JaroWinkler(mode='Jaro')
+higuera_mico = HigueraMico()
+sift4 = Sift4()
+eudex = Eudex()
+aline = ALINE()
+phonetic_edit = PhoneticEditDistance()
+algos = [iss, bisim, dlev, prefix, lcs, mlipns, strcmp95, mra, editex, saps, flexmetric, jaro, higuera_mico, sift4, eudex,
+     aline, phonetic_edit]
+
+algo_names = ['iterativesubstring', 'bisim', 'discountedlevenshtein', 'prefix', 'lcsstr', 'mlipns', 'strcmp95', 'mra',
+          'editex', 'saps', 'flexmetric', 'jaro', 'higueramico', 'sift4', 'eudex', 'aline',
+          'phoneticeditdistance']
+
+
+def featurize(df):
+    if len(df.columns)==3:
+        df.columns=['a', 'b', 'target']
+    elif len(df.columns)==2:
+        df.columns=['a', 'b']
+    else:
+        df = df.rename(columns={df.columns[0]: 'a', df.columns[1]: 'b' })
+        
+    df['TM_A'] = df.apply(lambda row: re.sub(
+        '[^a-zA-Z]+', '', unidecode.unidecode(row['a']).lower()), axis=1)
+    df['TM_B'] = df.apply(lambda row: re.sub(
+        '[^a-zA-Z]+', '', unidecode.unidecode(row['b']).lower()), axis=1)
+    
+
+    df['partial'] = df.apply(lambda row: fuzz.partial_ratio(row.TM_A,row.TM_B), axis=1)
+    df['tkn_sort'] = df.apply(lambda row: fuzz.token_sort_ratio(row.TM_A,row.TM_B), axis=1)
+    df['tkn_set'] = df.apply(lambda row: fuzz.token_set_ratio(row.TM_A,row.TM_B), axis=1)
+    
+	# df['sum_ipa'] = df.apply(lambda row: sum_ipa(row.TM_A,row.TM_B), axis=1)
+    
+    # Jellyfish levenshtein
+    df['levenshtein']= df.apply(lambda row: jellyfish.levenshtein_distance(row.TM_A,row.TM_B), axis=1)
+    # Scale Levenshtein column
+    scaler = MinMaxScaler()
+    df['levenshtein'] = scaler.fit_transform(df['levenshtein'].values.reshape(-1,1))
+
+    # Jellyfish phoneme
+    df['metaphone'] = df.apply(
+        lambda row: 1 if jellyfish.metaphone(row.TM_A)==jellyfish.metaphone(row.TM_B) else 0, axis=1)
+    df['nysiis'] = df.apply(
+        lambda row: 1 if jellyfish.nysiis(row.TM_A)==jellyfish.nysiis(row.TM_B) else 0, axis=1)
+    df['mtch_rtng_cdx'] = df.apply(
+        lambda row: 1 if jellyfish.match_rating_codex(row.TM_A)==jellyfish.match_rating_codex(row.TM_B) else 0, axis=1)
+    
+    df['pshp_soundex_first'] = df.apply(
+        lambda row: 1 if pshp_soundex_first.encode(row.TM_A)==pshp_soundex_first.encode(row.TM_B) else 0, axis=1)
+    
+    for i, algo in enumerate(algos):
+            df[algo_names[i]] = df.apply(lambda row: algo.sim(row.TM_A, row.TM_B), axis=1)
+    
+    return df
+
+
 
 def main():
 	""""A NLP app to identify infringing trademarks using spaCy"""
@@ -52,97 +137,7 @@ def main():
 #     1900, 2020, (1950, 2000)
 # )
 
-	def get_ratio(row):
-	    name = row['wordmark']
-	    return fuzz.token_sort_ratio(name, clean_text)
-
-	def make_clickable(val):
-    		return '<a href="{}">{}</a>'.format(val,val)
-
-    # load Model For Gender Prediction
-	TM_GBC_model = open("Data.nosync/TM_Gradient_boost_base_model.pkl","rb")
-	TM_clf = joblib.load(TM_GBC_model)
-
-	# Prediction
-	def predict_TM_outcome(data):
-		result = TM_clf.predict(data)
-		return result
-
-	# Featurizer
-	
-	pshp_soundex_first = PSHPSoundexFirst()
-	pe = Ainsworth()	
-	iss = IterativeSubString()
-	bisim = BISIM()
-	dlev = DiscountedLevenshtein()
-	prefix = Prefix()
-	lcs = LCSstr()
-	mlipns = MLIPNS()
-	strcmp95 = Strcmp95()
-	mra = MRA()
-	editex = Editex()
-	saps = SAPS()
-	flexmetric = FlexMetric()
-	jaro = JaroWinkler(mode='Jaro')
-	higuera_mico = HigueraMico()
-	sift4 = Sift4()
-	eudex = Eudex()
-	aline = ALINE()
-	phonetic_edit = PhoneticEditDistance()
-	algos = [iss, bisim, dlev, prefix, lcs, mlipns, strcmp95, mra, editex, saps, flexmetric, jaro, higuera_mico, sift4, eudex,
-         aline, phonetic_edit]
-
-	algo_names = ['iterativesubstring', 'bisim', 'discountedlevenshtein', 'prefix', 'lcsstr', 'mlipns', 'strcmp95', 'mra',
-              'editex', 'saps', 'flexmetric', 'jaro', 'higueramico', 'sift4', 'eudex', 'aline',
-              'phoneticeditdistance']
-
-
-	def featurize(df):
-	    if len(df.columns)==3:
-	        df.columns=['a', 'b', 'target']
-	    elif len(df.columns)==2:
-	        df.columns=['a', 'b']
-	    else:
-	        df = df.rename(columns={df.columns[0]: 'a', df.columns[1]: 'b' })
-	        
-	    df['TM_A'] = df.apply(lambda row: re.sub(
-	        '[^a-zA-Z]+', '', unidecode.unidecode(row['a']).lower()), axis=1)
-	    df['TM_B'] = df.apply(lambda row: re.sub(
-	        '[^a-zA-Z]+', '', unidecode.unidecode(row['b']).lower()), axis=1)
-	    
-
-	    df['partial'] = df.apply(lambda row: fuzz.partial_ratio(row.TM_A,row.TM_B), axis=1)
-	    df['tkn_sort'] = df.apply(lambda row: fuzz.token_sort_ratio(row.TM_A,row.TM_B), axis=1)
-	    df['tkn_set'] = df.apply(lambda row: fuzz.token_set_ratio(row.TM_A,row.TM_B), axis=1)
-	    
-		# df['sum_ipa'] = df.apply(lambda row: sum_ipa(row.TM_A,row.TM_B), axis=1)
-	    
-	    # Jellyfish levenshtein
-	    df['levenshtein']= df.apply(lambda row: jellyfish.levenshtein_distance(row.TM_A,row.TM_B), axis=1)
-	    # Scale Levenshtein column
-	    scaler = MinMaxScaler()
-	    df['levenshtein'] = scaler.fit_transform(df['levenshtein'].values.reshape(-1,1))
-
-	    # Jellyfish phoneme
-	    df['metaphone'] = df.apply(
-	        lambda row: 1 if jellyfish.metaphone(row.TM_A)==jellyfish.metaphone(row.TM_B) else 0, axis=1)
-	    df['nysiis'] = df.apply(
-	        lambda row: 1 if jellyfish.nysiis(row.TM_A)==jellyfish.nysiis(row.TM_B) else 0, axis=1)
-	    df['mtch_rtng_cdx'] = df.apply(
-	        lambda row: 1 if jellyfish.match_rating_codex(row.TM_A)==jellyfish.match_rating_codex(row.TM_B) else 0, axis=1)
-	    
-	    df['pshp_soundex_first'] = df.apply(
-	        lambda row: 1 if pshp_soundex_first.encode(row.TM_A)==pshp_soundex_first.encode(row.TM_B) else 0, axis=1)
-	    
-	    for i, algo in enumerate(algos):
-	            df[algo_names[i]] = df.apply(lambda row: algo.sim(row.TM_A, row.TM_B), axis=1)
-
-	    
-	    return df
-
-
 	# Streamlit App
-
 	menu = ["Levenshtein", "Similarity", "Phoneme"]
 	choice = st.sidebar.selectbox("How do you want your trademark to be evaluated", menu)
 
@@ -154,7 +149,10 @@ def main():
 		tokens = nlp(clean_text)
 		if st.button("Find Similar Trademarks"):
 
-			# spaCy tokens
+			def get_ratio(row):
+				    name = row['wordmark']
+				    return fuzz.token_sort_ratio(name, clean_text)
+				# spaCy tokens
 			spacy_streamlit.visualize_tokens(tokens)
 
 			# Import TM data
@@ -188,9 +186,12 @@ def main():
 			df_GB = df_matches[['wordmark']]
 			df_GB['Input'] = clean_text
 			df_GB = featurize(df_GB)
+			df_GB_features = df_GB.drop(['a', 'b', 'TM_A', 'TM_B'],1)
+			predict_score = predict_TM_outcome(df_GB_features)
 
 			# Return df
 			st.dataframe(df_GB)
+			st.dataframe(predict_score)
 
 
 	elif choice == "Similarity":
